385856bb81889019028c3d90fb4086333d1cfd5c
Willian Mazzorana <willianmazzorana@hotmail.com>
2021-09-07 20:35:23 -0300

    Criado interface Windows Forms

---
+++ Calcprod/FormCalcular.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Calcprod
+{
+    partial class FormCalcular
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProdutos = new System.Windows.Forms.DataGridView();
+            this.colCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDescricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colQtd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.txtCodProduto = new System.Windows.Forms.TextBox();
+            this.lblNomeProduto = new System.Windows.Forms.Label();
+            this.txtQtd = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProdutos
+            // 
+            this.dgvProdutos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProdutos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colCodigo,
+            this.colDescricao,
+            this.colQtd});
+            this.dgvProdutos.Location = new System.Drawing.Point(12, 70);
+            this.dgvProdutos.Name = "dgvProdutos";
+            this.dgvProdutos.RowHeadersVisible = false;
+            this.dgvProdutos.RowTemplate.Height = 25;
+            this.dgvProdutos.Size = new System.Drawing.Size(623, 220);
+            this.dgvProdutos.TabIndex = 4;
+            // 
+            // colCodigo
+            // 
+            this.colCodigo.HeaderText = "ID";
+            this.colCodigo.Name = "colCodigo";
+            this.colCodigo.ReadOnly = true;
+            // 
+            // colDescricao
+            // 
+            this.colDescricao.HeaderText = "Descrição";
+            this.colDescricao.Name = "colDescricao";
+            this.colDescricao.ReadOnly = true;
+            this.colDescricao.Width = 350;
+            // 
+            // colQtd
+            // 
+            this.colQtd.HeaderText = "Quantidade";
+            this.colQtd.Name = "colQtd";
+            this.colQtd.ReadOnly = true;
+            this.colQtd.Width = 150;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(95, 41);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 2;
+            this.btnAdicionar.Text = "&Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // txtCodProduto
+            // 
+            this.txtCodProduto.Location = new System.Drawing.Point(12, 12);
+            this.txtCodProduto.Name = "txtCodProduto";
+            this.txtCodProduto.PlaceholderText = "Código do produto";
+            this.txtCodProduto.Size = new System.Drawing.Size(119, 23);
+            this.txtCodProduto.TabIndex = 0;
+            // 
+            // lblNomeProduto
+            // 
+            this.lblNomeProduto.AutoSize = true;
+            this.lblNomeProduto.Enabled = false;
+            this.lblNomeProduto.Location = new System.Drawing.Point(137, 15);
+            this.lblNomeProduto.Name = "lblNomeProduto";
+            this.lblNomeProduto.Size = new System.Drawing.Size(142, 15);
+            this.lblNomeProduto.TabIndex = 3;
+            this.lblNomeProduto.Text = "</Descrição do produto>";
+            // 
+            // txtQtd
+            // 
+            this.txtQtd.Location = new System.Drawing.Point(12, 41);
+            this.txtQtd.Name = "txtQtd";
+            this.txtQtd.PlaceholderText = "Quantidade";
+            this.txtQtd.Size = new System.Drawing.Size(77, 23);
+            this.txtQtd.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(173, 41);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(120, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "&Calcular produção";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // FormCalcular
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 302);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtQtd);
+            this.Controls.Add(this.lblNomeProduto);
+            this.Controls.Add(this.txtCodProduto);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.dgvProdutos);
+            this.Name = "FormCalcular";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calcular Produção";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProdutos;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.TextBox txtCodProduto;
+        private System.Windows.Forms.Label lblNomeProduto;
+        private System.Windows.Forms.TextBox txtQtd;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCodigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDescricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colQtd;
+    }
+}
+
---
+++ Calcprod/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcprod
+{
+    public class Produto
+    {
+        public Produto(int id, double qtd, string nome)
+        {
+            Id = id;
+            Qtd = qtd;
+            Nome = nome;
+        }
+
+        public int Id { get; set; }
+        public double Qtd { get; set; }
+        public string Nome { get; set; }
+    }
+}
