3729fc1db6ff08e86bdca1b847b65c913ee5746e
Willian Mazzorana <willianmazzorana@hotmail.com>
2021-10-08 21:32:05 -0300

    Adicionado leitura de arquivo json

---
+++ CalcularProducao/Configuration.cs
@@ -0,0 +1,14 @@
+﻿namespace CalcularProducao
+{
+    public class Configuration
+    {
+        public Configuration(string caminhoPlanilha, string caminhoArquivoTXT)
+        {
+            CaminhoPlanilha = caminhoPlanilha;
+            CaminhoArquivoTXT = caminhoArquivoTXT;
+        }
+
+        public string CaminhoPlanilha { get; set; }
+        public string CaminhoArquivoTXT { get; set; }
+    }
+}
--- CalcularProducao/Program.cs
+++ CalcularProducao/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ClosedXML.Excel;
 using Figgle;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,8 +9,11 @@ namespace CalcularProducao
 {
     class Program
     {
+        public static string _urlPlanilha;
+        public static string _urlArquivoTXT;
+        
         static void Main(string[] args)
-        {
+        {   
             Console.WriteLine(" 2021 - Willian Software");
             Console.WriteLine(" Calculo de Producao");
             Console.WriteLine(" Versão 1.0");
@@ -18,12 +22,16 @@ namespace CalcularProducao
             Animation();
             Console.WriteLine("ATENÇÃO: Programa em fase de desenvolvimento!");
             Console.WriteLine("");
+            //CreateFileJsonConfig();
+            ReadFileJsonConfig();
+            Console.WriteLine("");
 
             try
             {
-                var arquivoXLS = Properties.Resources.URL_PLANILHA;
+                var arquivoXLS = _urlPlanilha;
                 //abrir planilha
                 Console.WriteLine("Lendo planilha de dados ............");
+                Console.WriteLine("");
                 var workBook = new XLWorkbook(@arquivoXLS);
                 var planilha = workBook.Worksheet(1);
                 //inicia da linha 3 da planilha, pois a 1 é cabecalho e a 2 os titulos das colunas
@@ -63,7 +71,7 @@ namespace CalcularProducao
             }
             catch(Exception ex)
             {
-                Console.WriteLine($"Ocorreu um erro! Detalhes do ocorrido: {ex.Message}");
+                Console.WriteLine($"Erro na leitura da planilha de dados: {ex.Message}");
             }
             finally
             {
@@ -75,8 +83,7 @@ namespace CalcularProducao
 
         public static void CreateFileTXT(List<Produto> produtos)
         {
-            var arquivoTXT = Properties.Resources.URL_ARQUIVO_TXT;
-            StreamWriter sw = new(@arquivoTXT);            
+            StreamWriter sw = new(_urlArquivoTXT);
             sw.WriteLine($"Arquivo gerado em { DateTime.Now }");
 
             try
@@ -92,12 +99,12 @@ namespace CalcularProducao
 
                 sw.WriteLine("\n");
                 sw.WriteLine("---------------- END ----------------");
-                sw.WriteLine($"CalcProd 1.0");
+                sw.WriteLine($"CALCPROD 1.0");
                 sw.Close();
             }
             catch(Exception ex)
             {
-                Console.WriteLine($"Ocorreu um erro. Detalhes do ocorrido: { ex.Message }");
+                Console.WriteLine($"Erro na criação do arquivo com os calculos de produção: { ex.Message }");
             }
             finally
             {
@@ -107,8 +114,47 @@ namespace CalcularProducao
 
         public static void Animation()
         {
-            Console.WriteLine(FiggleFonts.Ivrit.Render("Willian"));
+            Console.WriteLine(FiggleFonts.Ivrit.Render("Willian 2021"));
         }
 
+        public static void ReadFileJsonConfig()
+        {                    
+            try
+            {
+                //pega o caminho do executavel
+                string path = AppDomain.CurrentDomain.BaseDirectory.ToString();
+                //abre o arquivo e le o json
+                using StreamReader file = File.OpenText(@$"{path}\config.txt");
+                JsonSerializer jsonSerializer = new JsonSerializer();
+                //deserealiza o json para o objeto de configuracao
+                Configuration configuration = (Configuration)jsonSerializer.Deserialize(file, typeof(Configuration));
+                //atribui as variaveis os valores carregados do arquivo json                
+                _urlArquivoTXT = configuration.CaminhoArquivoTXT;
+                _urlPlanilha = configuration.CaminhoPlanilha;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Erro na leitura do arquivo Json: {ex.Message}");
+                CreateFileJsonConfig();
+            }
+        }
+
+        public static void CreateFileJsonConfig()
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory.ToString();
+                Configuration configuration = new Configuration(@"D:\WN\Sobras padaria.xlsx", @"C:\Users\Willian\Desktop\Calculo de Sobras.txt");
+                JsonSerializer serializer = new();
+                using StreamWriter sw = new StreamWriter(@$"{path}\config.txt");
+                using JsonWriter writer = new JsonTextWriter(sw);
+                serializer.Serialize(writer, configuration);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro na criação do arquivo Json: {ex.Message}");
+            }
+
+        }
     }
 }
